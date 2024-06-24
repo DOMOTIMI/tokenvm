5be91992dce5e77c2602c0736ee90ce3d4297b5d
DOMOTIMI <howellsSylvanus@gmail.com>
2024-06-24 17:11:12 +0000

    domotimi

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "Domotimi"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "howells"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "silva"
 )
 
 var ID ids.ID
--- registry/registry.go
+++ registry/registry.go
@@ -23,6 +23,8 @@ func init() {
 	errs.Add(
 		// When registering new actions, ALWAYS make sure to append at the end.
 		consts.ActionRegistry.Register(&actions.Transfer{}, actions.UnmarshalTransfer, false),
+		consts.ActionRegistry.Register(&actions.CreateAsset{}, actions.UnmarshalCreateAsset, false),
+		consts.ActionRegistry.Register(&actions.MintAsset{}, actions.UnmarshalMintAsset, false),
 
 		// TODO: register action: actions.CreateAsset
 		// TODO: register action: actions.MintAsset
@@ -31,9 +33,11 @@ func init() {
 
 		consts.ActionRegistry.Register(&actions.CreateOrder{}, actions.UnmarshalCreateOrder, false),
 		consts.ActionRegistry.Register(&actions.FillOrder{}, actions.UnmarshalFillOrder, false),
-		consts.ActionRegistry.Register(&actions.CloseOrder{}, actions.UnmarshalCloseOrder, false),
+		consts.ActionRegistry.Register(&actions.CloseOrder{}
+			, actions.UnmarshalCloseOrder, false),
 
-		consts.ActionRegistry.Register(&actions.ImportAsset{}, actions.UnmarshalImportAsset, true),
+		consts.ActionRegistry.Register(&actions.ImportAsset{}, acti
+			ons.UnmarshalImportAsset, true),
 		consts.ActionRegistry.Register(&actions.ExportAsset{}, actions.UnmarshalExportAsset, false),
 
 		// When registering new auth, ALWAYS make sure to append at the end.
